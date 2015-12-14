f92a88bacefe58c7a76b864e70d947d755bfed25
Juan Manuel Berros <juanmaberros@gmail.com>
2015-12-13 23:35:10 -0300

    i3pystatus mail_check rescue empty file

--- i3pystatus_modules/mail_check.py
+++ i3pystatus_modules/mail_check.py
@@ -4,15 +4,15 @@ import subprocess
 
 class MailCheck(IntervalModule):
     def run(self):
-        output = subprocess.check_output(["grep", "Subject", "/var/mail/juan"])
-        mails = [mail for mail in output.decode("utf-8").split("\n")
-                 if "INTERNAL DATA" not in mail and len(mail) > 0]
+        try:
+            output = subprocess.check_output(["grep", "Subject", "/var/mail/juan"])
+            mails = [mail for mail in output.decode("utf-8").split("\n")
+                    if "INTERNAL DATA" not in mail and len(mail) > 0]
 
-        count = len(mails)
-        msg = "" if count == 0 else "You have mail! ({})".format(count)
-
-        with open("/home/juan/testing_mail_script", "w") as f:
-            f.write(str(mails))
+            count = len(mails)
+            msg = "" if count == 0 else "You have mail! ({})".format(count)
+        except subprocess.CalledProcessError:
+            msg = ""
 
         self.output = {
             "full_text": msg,
