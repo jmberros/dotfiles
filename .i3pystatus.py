553c59aaa08fe327822b3498958c3c12276b0956
Juan Manuel Berros <juanmaberros@gmail.com>
2016-05-16 21:30:45 -0300

    i3pystatus from beleriand

--- .i3pystatus.py
+++ .i3pystatus.py
@@ -3,52 +3,49 @@
 import subprocess
 
 from i3pystatus import Status
-from i3pystatus.updates import aptget
+#  from i3pystatus.updates import aptget
 
 status = Status(standalone=True)
 
-status.register("weather",
-                location_code="ARXX2499:1:AR",
-                interval=300,
-                format="{current_temp} {humidity}%"
-                )
+#  status.register("weather",
+                #  location_code="ARXX2499:1:AR",
+                #  interval=300,
+                #  format="{current_temp} {humidity}%"
+                #  )
 
 status.register("clock",
                 color="#cccccc",
                 format="%H:%M, %A %-d %B",)
 
-status.register("updates",
-                format = "{count} update(s) available",
-                format_no_updates = "",
-                backends = [aptget.AptGet()])
+#  status.register("updates",
+                #  format = "{count} update(s) available",
+                #  format_no_updates = "",
+                #  backends = [aptget.AptGet()])
 
 status.register("mail_check")
+status.register("restart_required")
 
 status.register("pulseaudio",
                 format="🔊 {volume}",)
 
 status.register("pomodoro",
-                long_break_duration=30,
+                long_break_duration=(20*60),
                 sound="/home/juan/Dropbox/glass_ping.wav")
-
-status.register("radiotray",
-                color="#f9ef8d")
-
-status.register("now_playing",
-    player="clementine",
-    color="#ffc080",
-    format='{artist}: "{title}" {song_elapsed}/{song_length}')
 
 status.register("load",
                 color="#bbbbbb",
                 critical_limit=8,
                 format="💻 {avg1} / {avg5}")
 
-#  status.register("mem_bar",
-                #  multi_colors=True,
-                #  color="#333333",
-                #  warn_color="#cccccc",
-                #  alert_color="#ffffff")
+status.register("mem",
+                divisor=10**9,
+                color="#4CAF50",
+                format="{used_mem} / {avail_mem} Gb")
+
+status.register("now_playing",
+    player="clementine",
+    color="#ffc080",
+    format='{artist}: "{title}" {song_elapsed}/{song_length}')
 
 status.register("network",
     interface="enp0s25",
@@ -72,20 +69,23 @@ status.register("network",
 # Shows disk usage of /
 # Format:
 # 42/128G [86G]
-#  status.register("disk",
-    #  path="/",
-    #  color="#bbbbbb",
-    #  #format="{used} / {total}G [ {avail}G ]",)
-    #  format="{avail}G ({total}G)",)
+status.register("disk",
+    path="/",
+    color="#bbbbbb",
+    #format="{used} / {total}G [ {avail}G ]",)
+    format="🏠 {avail}G",)
 
-#  status.register("disk",
-    #  color="#bbbbbb",
-    #  path="/media/500gb",
-    #  format="{avail}G ({total}G)")
+status.register("disk",
+    color="#bbbbbb",
+    path="/media/new_500gb/",
+    format="🎜 {avail}G",)
 
-#  status.register("disk",
-    #  color="#bbbbbb",
-    #  path="/media/600gb",
-    #  format="{avail}G ({total}G)")
+status.register("disk",
+    color="#bbbbbb",
+    path="/media/old_500gb/",
+    format="{avail}G",)
+
+#  status.register("radiotray",
+                #  color="#f9ef8d")
 
 status.run()
