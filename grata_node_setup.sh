5145a2d73282721c970a38232ce3817102cfb73d
Juan Manuel Berros <juanmaberros@gmail.com>
2022-08-16 15:47:18 -0300

    updates on grata node setup

--- grata_node_setup.sh
+++ grata_node_setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
 apt install -y tmux vim
-pip install --upgrade pip
 pip install ipython
+
+tmux new-session
+
+alias pm="python manage.py"
+alias shell="python manage.py shell"
+
+echo
+echo
+echo "🤖 --( Welcome to the pod '`hostname`'! )"
+echo "- You are in a tmux session now. Ctrl-b is the prefix key."
+echo "  See https://tmuxcheatsheet.com/ for help or leave with Ctrl-b + D."
+echo "- run 'shell' to start a Django shell"
+echo "- 'pm' is 'python manage.py'"
+echo
+echo
