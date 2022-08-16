10a41a56366cd996a2df07bb4632e5f39576a983
Juan Manuel Berros <juanmaberros@gmail.com>
2022-08-16 15:57:45 -0300

    updates on grata node setup

--- grata_node_setup.sh
+++ grata_node_setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 apt install -y -qq tmux vim
+pip install --root-user-action=ignore --upgrade pip
 pip install --root-user-action=ignore -q ipython
 
 echo
 echo
-echo "🤖 --( Welcome to the pod '`hostname`'! )"
+echo "🤖 : Welcome to the pod '`hostname`'!"
 echo
 echo
