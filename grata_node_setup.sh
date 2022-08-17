729a1ade13e66233b9715f28c0dc15a6e1e38cde
Juan Manuel Berros <juanmaberros@gmail.com>
2022-08-17 11:46:41 -0300

    WIP

--- grata_node_setup.sh
+++ grata_node_setup.sh
@@ -2,8 +2,8 @@
 
 apt update
 apt install -y -qq tmux vim htop
-pip install --root-user-action ignore --upgrade pip
-pip install --root-user-action ignore -q ipython
+pip install --upgrade pip
+pip install -q ipython
 
 Purple='\033[0;35m'
 Color_Off='\033[0m'
