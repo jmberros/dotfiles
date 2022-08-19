a614c727fbf82f5112cedb53e1952ff80b97dc69
Juan Manuel Berros <juanmaberros@gmail.com>
2022-08-19 16:12:12 -0300

    more grata scripts

--- grata_node_setup.sh
+++ grata_node_setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 apt update
-apt install -y -qq tmux vim htop parallel
+apt install -y -qq tmux vim htop parallel tree zsh
 # pip install --upgrade pip
-pip install -q ipython
+pip install -q ipython jupyter
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 Purple='\033[0;35m'
 Color_Off='\033[0m'
