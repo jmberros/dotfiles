7aed0605983e4b28e41cbab2b22ede915e4f0558
Juan Manuel Berros <juanmaberros@gmail.com>
2023-08-09 18:19:10 -0300

    more updates polybar

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -8,3 +8,8 @@ while pgrep -x polybar >/dev/null; do sleep 0.1; done
 
 ~/repos/dotfiles/polybar/launch_polybar_main.sh
 ~/repos/dotfiles/polybar/launch_polybar_aux.sh
+
+# NOTE:
+# The backlight module needs permissions to write this system file:
+sudo chgrp juan /sys/class/backlight/intel_backlight/brightness
+sudo chmod g+w /sys/class/backlight/intel_backlight/brightness
