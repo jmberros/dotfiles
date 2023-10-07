2fcab7390087d79c31511a363eb63f97e3bb52df
Juan Manuel Berros <juanmaberros@gmail.com>
2023-10-06 22:03:26 -0300

    updates network polybar

--- scripts/rofi_google_search.sh
+++ scripts/rofi_google_search.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-notify-send "1"
 
 INPUT=$(rofi -dmenu -p " oogle search:")
-notify-send "2 $INPUT"
 if [[ -z $INPUT ]]; then
     exit 1
 fi
-notify-send "3"
 google-chrome "https://www.google.com/search?q=$INPUT"
-notify-send "4"
-
